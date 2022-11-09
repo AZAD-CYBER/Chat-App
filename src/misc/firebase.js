34120c2369e82e4fb4c8d18dc83b9beb4d09c013
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2022-11-09 15:43:29 +0530

    firebase-Setup

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyD4fR89UA_3_X_LBqOzzm_Glf9NpgkBFh4",
+
+  authDomain: "chat-web-app-83225.firebaseapp.com",
+
+  projectId: "chat-web-app-83225",
+
+  storageBucket: "chat-web-app-83225.appspot.com",
+
+  messagingSenderId: "12735673270",
+
+  appId: "1:12735673270:web:3cfd1c89b047fca7dec717",
+};
+const app = firebase.initializeApp(config);
