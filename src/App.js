d04861e2f5dac66af0a0358aec348a72b72f334e
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2022-11-09 16:49:47 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "rsuite/styles/index.less";
 import "./styles/main.scss";
+import { Switch } from "react-router";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
